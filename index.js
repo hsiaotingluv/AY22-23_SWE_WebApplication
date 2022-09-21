e7fd64da5e843a328de383a41c742a8ed34efbe4
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2022-09-21 20:05:02 +0800

    Disable cors

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Import express
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 
 // App Middleware
-const whitelist = ["http://localhost:3000"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const whitelist = ["http://localhost:3000"];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 // Import Body parser
 let bodyParser = require("body-parser");
 
@@ -26,7 +26,7 @@ const app = express();
 // Import routes
 const apiRoutes = require("./api-routes");
 const { application } = require("express");
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 // Configure bodyparser to handle post requests
 app.use(
   bodyParser.urlencoded({
@@ -53,35 +53,35 @@ app.listen(port, function () {
   console.log("Running RestHub on port " + port);
 });
 
-// Use Api routes in the App
-app.use("/api", apiRoutes);
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
+// Have heroku to serve a static build file
+if (process.env.NODE_ENV === "production") {
+  // Have Node serve the files for our built React app
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  // All other GET requests not handled before will return our React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+} else {
+  // Use Api routes in the App
+  app.use("/api", apiRoutes);
+  // Handle GET requests to /api route
+  app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+}
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// // All other GET requests not handled before will return our React app
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 
-// Send message for default URL
-app.get("/", (req, res) =>
-  res.send("Hello World with Express, deployed with Heroku!!!")
-);
-
-// // Have heroku to serve a static build file
-// if (process.env.NODE_ENV === "production") {
-//   // Have Node serve the files for our built React app
-//   app.use(express.static(path.resolve(__dirname, "./client/build")));
-//   // All other GET requests not handled before will return our React app
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-//   });
-// }
+// // Send message for default URL
+// app.get("/", (req, res) =>
+//   res.send("Hello World with Express, deployed with Heroku!!!")
+// );
 
 // Export our app for testing purposes
 module.exports = app;
